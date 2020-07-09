fe94b32553a29ee8b8d19a75643b6b5d696f0c5a
Andreas Boltres <andreas.boltres@posteo.de>
2020-07-09 10:15:44 +0200

    updated gitignore to ignore assets folder contents and python files

--- lib/etcpages/contribpage.dart
+++ lib/etcpages/contribpage.dart
@@ -2,42 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:seasoncalendar/helpers/styles.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:seasoncalendar/helpers/links.dart';
 
-final String contribContact = "Diese App ist noch sehr jung und kann noch"
-  " Fehler enthalten bzw. in sehr viele Richtungen erweitert werden. Wenn"
-  " du einen Fehler gefunden hast oder einen Verbesserungsvorschlag hast,"
-  " kontaktiere mich gerne über meine Website.";
+class ContribPage extends StatelessWidget {
 
-final String contribCode = "Der Quellcode dieser App ist auf GitHub"
-    " verfügbar.";
+  final Map<String, dynamic> _contribPageText;
+  final Map<String, dynamic> _links;
 
-class ContributePage extends StatelessWidget {
+  ContribPage(Map<String, dynamic> contribPageText, Map<String, dynamic> links)
+      : _contribPageText = contribPageText, _links = links;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text("Beitragen")
+          title: Text(_contribPageText['contribPageTitle'])
       ),
       body: Container(
         margin: EdgeInsets.all(18),
         child: Column(
           children: <Widget>[
-            Text("Etwas fehlt dir oder ist falsch?", style: font20b),
+            Text(_contribPageText['contribPageContactHeadline'], style: font20b),
             SizedBox(height: 20),
             RichText(
               textAlign: TextAlign.justify,
               text: TextSpan(
-                text: contribContact,
+                text: _contribPageText['contribPageContact'],
                 style: font18.copyWith(color: Colors.black),
               ),
             ),
             SizedBox(height: 10),
             RaisedButton(
-              child: Text("Zur Website"),
+              child: Text(_contribPageText['contribPageContactButtonText']),
               onPressed: () async {
-                final url = websiteContactPage;
+                final url = _links['websiteContactPage'];
                 if (await canLaunch(url)) {
                   await launch(
                     url,
@@ -47,20 +44,20 @@ class ContributePage extends StatelessWidget {
               },
             ),
             SizedBox(height: 50),
-            Text("Zeig mir den Code!", style: font20b),
+            Text(_contribPageText['contribPageCodeHeadline'], style: font20b),
             SizedBox(height: 20),
             RichText(
               textAlign: TextAlign.justify,
               text: TextSpan(
-                text: contribCode,
+                text: _contribPageText['contribPageCode'],
                 style: font18.copyWith(color: Colors.black),
               ),
             ),
             SizedBox(height: 10),
             RaisedButton(
-              child: Text("Zum GitHub-Repo"),
+              child: Text(_contribPageText['contribPageCodeButtonText']),
               onPressed: () async {
-                final url = seasonCalendarGithub;
+                final url = _links['seasonCalendarGithub'];
                 if (await canLaunch(url)) {
                   await launch(
                     url,
--- lib/helpers/links.dart
+++
@@ -1,3 +0,0 @@
-const String websiteContactPage = "https://flunzmas.com/contact/";
-const String websiteSupportPage = "https://flunzmas.com/support/";
-const String seasonCalendarGithub = "https://github.com/Flunzmas/seasoncalendar";
