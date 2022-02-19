4c04127a307dec2e08d67c9429491c670dea3e4b
Matthias Schmitt <male.schmitt@posteo.de>
2022-02-19 19:53:15 +0100

    [CI] Rename apks

---
+++ rename_apks.sh
@@ -0,0 +1,16 @@
+export BUILD_NUMBER=$(sed -n 's/version: \([^+]\+\)+\([0-9]\+\)/\2/p' pubspec.yaml)
+export BUILD_VERSION=$(sed -n 's/version: \([^+]\+\)+\([0-9]\+\)/\1/p' pubspec.yaml)
+export BUILD_NAME=${BUILD_VERSION//[.]/-}
+
+f=build/app/outputs/flutter-apk/app-foss-release.apk
+name=build/app/outputs/flutter-apk/seasoncalendar_v${BUILD_NAME}_c${BUILD_NUMBER}.apk
+
+mv $f $name
+
+for f in build/app/outputs/flutter-apk/app*release.apk; do
+    name=$(sed -e "s/-foss-release//" <<< $f | \
+           sed -e "s/app-/app/" | \
+           sed -e "s/app-\?/seasoncalendar_v${BUILD_NAME}_c${BUILD_NUMBER}_/2"\
+        )
+    mv $f $name
+done
