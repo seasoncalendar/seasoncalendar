32c2c9dc7446a5e40fff50f816f4c558894620c0
Matthias Schmitt <male.schmitt@posteo.de>
2022-03-05 03:36:46 +0100

    Refactor month selector

--- lib/components/month_selector.dart
+++ lib/components/month_selector.dart
@@ -8,8 +8,46 @@ class MonthSelector extends StatelessWidget {
 
   const MonthSelector(FoodDisplayConfiguration fdc, {Key? key}) : _fdc = fdc, super(key: key);
 
+  Widget monthDialog(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.white,
+      content: SizedBox(
+        width: double.minPositive,
+        child: ListView.builder(
+            itemCount: 12 * 2 - 1,
+            // 12 months in the gregorian calendar + dividers
+            itemBuilder: (context, i) {
+              if (i % 2 != 0) {
+                return const Divider(height: 0);
+              }
+              int monthIndex = (i / 2).round();
+              return ListTileTheme(
+                selectedTileColor: Theme.of(context).colorScheme.primary,
+                selectedColor: Colors.black,
+                child: ListTile(
+                  title: Text(
+                    getMonthNameFromIndex(context, monthIndex),
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  selected: monthIndex == _fdc.monthIndex,
+                  onTap: () {
+                    _fdc.setMonth(monthIndex);
+                    Navigator.of(context).pop();
+                  },
+                ),
+              );
+            }),
+      ),
+      elevation: 10,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return Swipe(
       onSwipeLeft: () {
         _fdc.shiftMonth(1);
@@ -22,79 +60,38 @@ class MonthSelector extends StatelessWidget {
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Container(
-              child: IconButton(
-                  icon: const Icon(
-                    Icons.arrow_back,
-                  ),
-                  onPressed: () {
-                    _fdc.shiftMonth(-1);
-                  }),
-            ),
-            Expanded(
-                child: Container(
-                    child: InkWell(
-                        child: Text(
-                          getMonthNameFromIndex(context, _fdc.monthIndex),
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        onTap: () {
-                          showDialog(
-                            context: context,
-                            builder: (_) => AlertDialog(
-                              backgroundColor: Colors.white,
-                              content: SizedBox(
-                                width: double.minPositive,
-                                child: ListView.builder(
-                                    shrinkWrap: true,
-                                    itemCount: 12 * 2 - 1,
-                                    // 12 months in the gregorian calendar
-                                    itemBuilder: (context, i) {
-                                      if (i % 2 == 0) {
-                                        return ListTileTheme(
-                                          selectedTileColor: Theme.of(context)
-                                              .colorScheme
-                                              .primary,
-                                          selectedColor: Colors.black,
-                                          child: ListTile(
-                                            title: Text(
-                                              getMonthNameFromIndex(
-                                                  context, (i / 2).round()),
-                                              textAlign: TextAlign.center,
-                                              style: const TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                            selected: i / 2 == _fdc.monthIndex,
-                                            onTap: () {
-                                              _fdc.setMonth((i / 2).round());
-                                              Navigator.of(context).pop();
-                                            },
-                                          ),
-                                        );
-                                      } else {
-                                        return const Divider(
-                                          height: 0,
-                                        );
-                                      }
-                                    }),
-                              ),
-                              elevation: 10,
-                            ),
-                            barrierDismissible: true,
-                          );
-                        }))),
-            Container(
-              child: IconButton(
+            IconButton(
                 icon: const Icon(
-                  Icons.arrow_forward,
-                  color: Colors.black,
+                  Icons.arrow_back_ios,
                 ),
                 onPressed: () {
-                  _fdc.shiftMonth(1);
-                },
+                  _fdc.shiftMonth(-1);
+                }),
+            Expanded(
+                child: InkWell(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                        getMonthNameFromIndex(context, _fdc.monthIndex),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    onTap: () {
+                      showDialog(
+                        context: context,
+                        builder: monthDialog,
+                        barrierDismissible: true,
+                      );
+                    })),
+            IconButton(
+              icon: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.black,
               ),
+              onPressed: () {
+                _fdc.shiftMonth(1);
+              },
             ),
           ],
         ),
